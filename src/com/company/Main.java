87161e10795bc42e0f5801d9718fef74562542a4
qinaleon1998 <qianleon@outlook.com>
2021-04-01 10:45:25 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int result = BinarySearcher.findMidpoint(1,9);
+    System.out.println(result);
+
+    int result2 = BinarySearcher.findMidpoint(1,100);
+    System.out.println(result2);
+    }
+}
